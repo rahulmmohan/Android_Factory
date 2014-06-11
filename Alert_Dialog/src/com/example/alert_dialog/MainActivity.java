f2a6091594e5e0141b246372cc262937b27402e2
Rahul M Mohan <rahulmohanpgr@gmail.com>
2014-06-11 16:33:20 -0700

    Alerts

---
+++ Alert_Dialog/src/com/example/alert_dialog/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.alert_dialog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button b1 = (Button) findViewById(R.id.b1);
+		Button b2 = (Button) findViewById(R.id.b2);
+	
+		b1.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				AlertDialog.Builder ab=new AlertDialog.Builder(MainActivity.this);
+				ab.setTitle("Alert Dialog");
+				ab.setMessage("Are you ok?");
+				ab.setPositiveButton("Ok", null);
+			
+				ab.show();		
+			}
+		});
+		b2.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), "this is toast ", 2000)
+						.show();
+
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
